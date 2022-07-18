9b9dd1a6cca133753dadd864524b63c8a062a8e6
rhinoleo <renaud.denier@gmail.com>
2022-07-18 20:08:35 +0200

    Created the class StatementPrinter and Console to make test pass

---
+++ src/main/java/hexagonal/architecture/application/domain/Console.java
@@ -0,0 +1,10 @@
+package hexagonal.architecture.application.domain;
+
+public class Console {
+
+	public void printLine(String statementHeader) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/hexagonal/architecture/application/domain/StatementPrinter.java
@@ -0,0 +1,18 @@
+package hexagonal.architecture.application.domain;
+
+import java.util.List;
+
+public class StatementPrinter {
+	
+	public static final String STATEMENT_HEADER = "OPERATION  | DATE       | AMOUNT  | BALANCE";
+
+	public StatementPrinter(Console console) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void print(List<Transaction> noTransacions) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
