8f1274762910034781f2c15e708f8de745e650a2
rhinoleo <renaud.denier@gmail.com>
2022-07-18 19:54:15 +0200

    Created class Transaction and Transactions used in the TransactionsTest

---
+++ src/main/java/hexagonal/architecture/application/domain/Transaction.java
@@ -0,0 +1,11 @@
+package hexagonal.architecture.application.domain;
+
+import java.math.BigDecimal;
+
+public class Transaction {
+
+	public Transaction(String string, BigDecimal valueOf, BigDecimal valueOf2) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/hexagonal/architecture/application/domain/Transactions.java
@@ -0,0 +1,17 @@
+package hexagonal.architecture.application.domain;
+
+import java.util.List;
+
+public class Transactions {
+
+	public void add(Transaction deposit) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Transaction> all() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
