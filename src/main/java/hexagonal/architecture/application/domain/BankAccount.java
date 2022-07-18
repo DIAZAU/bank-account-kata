9a4ffd9298c29e47fa51af87082f234dcc8a3f89
rhinoleo <renaud.denier@gmail.com>
2022-07-18 23:14:31 +0200

    Created class BankAccount and Clock used in the TransactionsTest

---
+++ src/main/java/hexagonal/architecture/application/domain/BankAccount.java
@@ -0,0 +1,16 @@
+package hexagonal.architecture.application.domain;
+
+import java.math.BigDecimal;
+
+public class BankAccount {
+
+	public BankAccount(Transactions transactions, StatementPrinter statementPrinter, BigDecimal valueOf) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void deposit(String systemDate, BigDecimal valueOf) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/hexagonal/architecture/application/domain/Clock.java
@@ -0,0 +1,10 @@
+package hexagonal.architecture.application.domain;
+
+public class Clock {
+
+	public Object dateAsString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
