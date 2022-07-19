1c81e1a098987299a3f98ccb4bd2b24febfa737e
rhinoleo <renaud.denier@gmail.com>
2022-07-19 13:00:42 +0200

    Refactored the code into BankAccountController

--- src/main/java/hexagonal/architecture/adapter/api/BankAccountController.java
+++ src/main/java/hexagonal/architecture/adapter/api/BankAccountController.java
@@ -2,6 +2,7 @@ package hexagonal.architecture.adapter.api;
 
 import java.math.BigDecimal;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,22 +14,22 @@ import hexagonal.architecture.application.port.in.WithdrawUseCase;
 @RequestMapping("/account")
 public class BankAccountController {
 
-	private DepositUseCase depositUseCase;
-	private final WithdrawUseCase withdrawUseCase;
+    private final DepositUseCase depositUseCase;
+    private final WithdrawUseCase withdrawUseCase;
 
-	public BankAccountController(DepositUseCase depositUseCase, WithdrawUseCase withdrawUseCase) {
-		this.depositUseCase = depositUseCase;
-		this.withdrawUseCase = withdrawUseCase;
-	}
+    public BankAccountController(DepositUseCase depositUseCase, WithdrawUseCase withdrawUseCase) {
+        this.depositUseCase = depositUseCase;
+        this.withdrawUseCase = withdrawUseCase;
+    }
 
-	@PostMapping(value = "/{id}/deposit/{amount}")
-	public Boolean deposit(long id, BigDecimal amount) {
-		return depositUseCase.deposit(id, amount);
-	}
+    @PostMapping(value = "/{id}/deposit/{amount}")
+    Boolean deposit(@PathVariable final Long id, @PathVariable final BigDecimal amount) {
+        return depositUseCase.deposit(id, amount);
+    }
 
-	@PostMapping(value = "/{id}/withdraw/{amount}")
-	public Boolean withdraw(long i, BigDecimal valueOf) {
-		return withdrawUseCase.withdraw(i, valueOf);
-	}
-
+    @PostMapping(value = "/{id}/withdraw/{amount}")
+    Boolean withdraw(@PathVariable final Long id, @PathVariable final BigDecimal amount) {
+        return withdrawUseCase.withdraw(id, amount);
+    }
 }
+
