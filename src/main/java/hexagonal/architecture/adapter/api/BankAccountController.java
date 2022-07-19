b7c4575b53c711518031d1a1d6ec376459f3fd1e
rhinoleo <renaud.denier@gmail.com>
2022-07-19 12:27:06 +0200

    Created the class BankAccountController to make test pass

---
+++ src/main/java/hexagonal/architecture/adapter/api/BankAccountController.java
@@ -0,0 +1,12 @@
+package hexagonal.architecture.adapter.api;
+
+import hexagonal.architecture.application.port.in.DepositUseCase;
+import hexagonal.architecture.application.port.in.WithdrawUseCase;
+
+public class BankAccountController {
+
+	public BankAccountController(DepositUseCase depositUseCase, WithdrawUseCase withdrawUseCase) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
